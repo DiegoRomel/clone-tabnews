3cc99b5b48f37362cc4316b675720a61d1f1dffb
Diego.Romel <104534511+DiegoRomel@users.noreply.github.com>
2024-07-13 01:05:57 +0000

    modificacao na index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home () {
-    return <h1>Everyone has the potential for changing things, don't be afraid and do everything you can!</h1>
+    return <h1>Everyone has the potential for changing things, don't be afraid and do everything you can! Keep doing your best!</h1>
 }
 export default Home;
