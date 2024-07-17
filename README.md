800e9ac7cc9f216b27a877c8506bad23e95323b2
Diego.Romel <104534511+DiegoRomel@users.noreply.github.com>
2024-07-17 04:34:15 +0000

    adiciona scripts lint:check e lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto para tryhardar programação e conseguir um trampo bom.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home () {
-    return <h1>Tome decisões conscientes, não viva a vida no automático, faça o bem quando puder e nunca pare de seguir em frente.
+function Home() {
+  return (
+    <h1>
+      Tome decisões conscientes, não viva a vida no automático, faça o bem
+      quando puder e nunca pare de seguir em frente.
     </h1>
+  );
 }
 export default Home;
